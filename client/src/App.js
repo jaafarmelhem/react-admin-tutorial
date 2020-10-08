b91fdf08324ef85127f6381846afc3fd543f2e4c
Jaafar Melhem <jaafar.melhem@almajmoua.org>
2020-10-08 13:22:16 +0300

    edit func done

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import { Admin,Resource } from 'react-admin'
 import restProvider from 'ra-data-simple-rest'
 import PostList from './components/PostList'
 import PostCreate from './components/PostCreate'
+import PostEdit from './components/PostEdit'
 
 function App() {
 
 return(
   <Admin dataProvider={restProvider('http://localhost:3000')}> 
-      <Resource name='posts' list={PostList} create={PostCreate} />
+      <Resource name='posts' list={PostList} create={PostCreate} edit={PostEdit}/>
   </Admin>
   )   
 }
