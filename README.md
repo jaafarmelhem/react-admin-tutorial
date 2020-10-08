cae90d78224add540e92da0c92bef6b833b1668c
Jaafar Melhem <jaafar.melhem@almajmoua.org>
2020-10-08 12:11:34 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# react-admin-example
+ create a react-admin example
+# 
+ I follow the tutorial: https://www.youtube.com/watch?v=HRmdj-HpJyE&ab_channel=TraversyMedia
---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Admin,Resource } from 'react-admin'
+import restProvider from 'ra-data-simple-rest'
+import PostList from './components/PostList'
+
+function App() {
+
+return(
+  <Admin dataProvider={restProvider('http://localhost:3000')}> 
+      <Resource name='posts' list={PostList} />
+  </Admin>
+  )   
+}
+
+export default App;
